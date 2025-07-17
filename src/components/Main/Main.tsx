c540cc3744bfb2a9ea0708652eef7886adcf4f16
bsnikhil-dev <nikhilbs1697@gmail.com>
2025-07-17 12:08:06 +0530

    Added Main and Navbar Components

---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,12 @@
+import type React from "react";
+import Navbar from "../Navbar/Navbar";
+
+const Main = ():React.ReactElement=>{
+    return(
+        <div>
+            <Navbar title="Welcome"/>
+        </div>
+    )
+}
+
+export default Main;
